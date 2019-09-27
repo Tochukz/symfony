bc871a94bfcf213cf9ee6bad63ae49ddf043b06e
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-09-27 05:46:54 +0200

    sapce app with symfony

--- space-symfony/src/Controller/ArticleController.php
+++ space-symfony/src/Controller/ArticleController.php
@@ -9,7 +9,26 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response("My first pageb already!");
+    }
 
+    /**
+     * @Route("/news/{slug}")
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug)
+    {
+        return new Response($slug);
+    }
 }
